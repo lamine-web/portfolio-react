df5e51baf11f73ab6ffad65a13ebe457f9c9bb3e
Lamine Kama <laminekama038@gmail.com>
2025-04-22 12:46:36 +0800

    modifier le nav-bar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,47 +1,44 @@
-import React from "react";
+import logo from '../assets/logo.png'; // adapte le chemin selon ton projet
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          MonSite
+        <a className="navbar-brand d-flex align-items-center" href="#">
+          {/* <img src={logo} alt="Logo" width="40" height="40" className="me-2" /> */}
+          Mon portfolio
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+       <div className="container">
+       <div className="container ">
+       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Accueil
-              </a>
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                À propos
-              </a>
+              <a className="nav-link" href="#">À propos</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Services
-              </a>
+              <a className="nav-link" href="#">Competences</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Contact
-              </a>
+              <a className="nav-link" href="#">Projects</a>
             </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+            
           </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
         </div>
+       </div>
+       </div>
       </div>
     </nav>
   );
