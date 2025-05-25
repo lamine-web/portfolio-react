e3ebfd72155e148e3bbdbfe3942b23f4b3cd9362
Lamine Kama <laminekama038@gmail.com>
2025-05-25 16:16:16 +0800

    evolution....

--- src/component/Acceuil.jsx
+++ src/component/Acceuil.jsx
@@ -8,7 +8,7 @@ export default function Acceuil() {
     <div>
    <Jumbotron />
    <Carrousel />
-   <Propos />
+   {/* <Propos /> */}
     </div>
   )
 }
--- src/component/Propos.jsx
+++ src/component/Propos.jsx
@@ -4,25 +4,20 @@ import { motion } from 'framer-motion';
 import img from '../assets/about.jpeg'
 
 export default function Propos() {
+  const imgg = 'https://images.unsplash.com/photo-1558174685-430919a96c8d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8d2VifGVufDB8fDB8fHww'
   return (
     <>
-    <div className='container-fluid about py-5'>
-       <motion.h2 className='text-center text-primary' style={{ textDecoration: 'underline'}}>
-        A propos 
-        </motion.h2> 
-        {/* <hr className="my-4" /> */}
-      <main className='main py-5 d-flex justify-content-around align-items-center' >
+    <div className=''>
+      <main className='main py-5 d-flex justify-content-around align-items-center' style={{backgroundImage: `url(${imgg})`,backgroundRepeat:'no-repeat',backgroundSize: 'cover',backgroundPosition:'center'}}>
      <motion.div
-  initial={{ x: "" }} className='w-50' style={{lineHeight: '2'}}>
-       Je suis <strong>lamine kama</strong>, étudiant en licence 2 et passionné par le front-end.
-      Ce portfolio regroupe les projets que j’ai réalisés durant ma formation et en auto-apprentissage.
-      Je travaille principalement avec React, Vite, HTML, CSS, et un peu de Laravel pour le back-end.
+  initial={{ x: "" }} className='text-white' style={{lineHeight: '2',padding: '100px 0',textAlign:'center'}}>
+       Je suis <strong>lamine kama</strong>, étudiant en licence 2 et passionné par le front-end. <br />
+      Ce portfolio regroupe les projets que j’ai réalisés durant ma formation et en auto-apprentissage. <br />
+      Je travaille principalement avec React, Vite, HTML, CSS, et un peu de Laravel pour le back-end. <br />
       Je cherche à progresser, à apprendre encore plus, et à intégrer des projets concrets.
       Merci de visiter mon portfolio !
      </motion.div>
      <div> 
-      <motion.img src={img} alt="about" style={{width: '500px'}} animate={{ opacity: 10 }}/>
-      {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, repellat.</p> */}
      </div>
       </main>
     </div>
