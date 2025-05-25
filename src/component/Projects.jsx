64640eec4063df476e1959f49b229cdb744f7ac2
Lamine Kama <laminekama038@gmail.com>
2025-05-25 16:26:11 +0800

    evolution....

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -2,8 +2,64 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <div>
-      
+    <div className='container py-4'>
+      <h2 style={{textAlign:'center'}}>Mes Projects</h2>
+      <div className="container-fluid row row-cols-1 row-cols-md-3 g-4 py-5">
+  <div className="col">
+    <div className="card h-100">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a short card.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
